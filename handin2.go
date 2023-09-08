0e989bd413cdf0bcc139205e45ab72ba0198fdf1
Christer Knorborg <christerknorborg@hotmail.com>
2023-09-08 18:32:59 +0200

    fandt endnu en bug. Seems to be working now

--- handin2.go
+++ handin2.go
@@ -29,12 +29,12 @@ type Bob struct {
 }
 
 func (d *Dealer) init() {
-	d.r = rand.Intn(8) // for i coordinate
-	d.s = rand.Intn(8) // for j coordinate
+	d.r = rand.Intn(8) // generate random number between 0 and 7 for i coordinate
+	d.s = rand.Intn(8) // generate random number between 0 and 7 for j coordinate
 
 	d.matrix_b = generateRandomMatrix() // Generate random 8x8 matrix for Bob
 
-	shiftMatrix := shiftMatrix(d.r, d.s) // Generate shift matrix
+	shiftMatrix := shiftMatrix(d.r, d.s) // Generate shift matrix from r and s, and bloodtype compatibility lookup table
 
 	d.matrix_a = XORMatrix(d.matrix_b, shiftMatrix) // Generate Alice's matrix by XOR'ing Bob's matrix with the shift matrix
 }
@@ -69,35 +69,39 @@ func (b *Bob) init(y int, matrix [8][8]bool, s int) {
 }
 
 func (a *Alice) send() int {
-	u := (int(a.x) + a.r) % 8 // Alice computes u = x + r mod 2n and sends u to Bob
-	a.u = u                   // Alice stores u
+	u := (int(a.x) + a.r) % 8 // Alice computes u = x + r mod n and sends u to Bob
+	a.u = u                   // Alice also stores u (to send to Bob later when immiating sending data on a channel)
 	return u
 }
 
+// Method that immitates Alice receiving data from Bob on a channel between Alice and Bob
 func (a *Alice) receive(v int, z_B bool) {
 	a.v = v
 	a.z_B = z_B
 }
 
 func (a *Alice) computeOutput() bool {
-	z := XOR(a.matrix_a[a.u][a.v], a.z_B) // Alice outputs z = M_A[u, v] ⊕ z_B u
+	z := XOR(a.matrix_a[a.u][a.v], a.z_B) // Alice outputs z = M_A[u, v] ⊕ z_B. Which is equal to f(x,y)
 	return z
 }
 
 func (b *Bob) send() (int, bool) {
-	v := (int(b.y) + b.s) % 8 // Bob computes v = y + s mod 2n,
-	z_B := b.matrix_b[v][b.s] // and z_B = M_B[u, v] and sends (v, z_B) to Alice
+	v := (int(b.y) + b.s) % 8 // Bob computes v = y + s mod n,
+	z_B := b.matrix_b[b.u][v] // and z_B = M_B[u, v] and sends (v, z_B) to Alice.
 	return v, z_B
 }
 
+// Method that immitates Bob receiving data from Alice on a channel between Alice and Bob
 func (b *Bob) receive(u int) {
 	b.u = u
 }
 
+// XOR function that returns true if x and y are different, and false if they are the same
 func XOR(x bool, y bool) bool {
 	return (x || y) && !(x && y)
 }
 
+// XORMatrix function that returns entry-wise XOR of two 8x8 matrices
 func XORMatrix(matrixX [8][8]bool, matrixY [8][8]bool) [8][8]bool {
 	resultMatrix := [8][8]bool{}
 	for i := 0; i < 8; i++ {
@@ -112,7 +116,7 @@ func shiftMatrix(r int, s int) [8][8]bool {
 	shiftMatrix := [8][8]bool{}
 	for i := 0; i < 8; i++ {
 		for j := 0; j < 8; j++ {
-			shiftMatrix[i][j] = bloodtype_compatibility[(i-r+8)%8][(j-s+8)%8] // need to add 8 to make sure we get positive numbers. Go does not support negative modulo
+			shiftMatrix[i][j] = bloodtype_compatibility[(i-r+8)%8][(j-s+8)%8] // Go does not support negative modulo (add 8 to get positive numbers)
 		}
 	}
 	return shiftMatrix
@@ -131,7 +135,6 @@ func generateRandomMatrix() [8][8]bool {
 			}
 		}
 	}
-
 	return matrix
 }
 
@@ -181,10 +184,31 @@ func main() {
 	b := Bob{}
 	b.init(int(BobBloodType), d.getMatrixB(), d.getS()) // Bob initializes his matrix and coordinates
 
-	b.receive(a.send()) // Alice sends u to Bob
+	b.receive(a.send()) // Immitate Alice sends u to Bob
 
-	v, z_B := b.send()
-	a.receive(v, z_B) // Bob sends v and z_B to Alice
+	v, z_B := b.send() // Immitate Bob sends v and z_B to Alice
+	a.receive(v, z_B)
+
+	fmt.Println("Dealer Information:")
+	fmt.Println("r:", d.r)
+	fmt.Println("s:", d.s)
+	fmt.Println("")
+
+	fmt.Println("Alice Information:")
+	fmt.Println("x:", a.x)
+	fmt.Println("r:", a.r)
+	fmt.Println("u:", a.u)
+	fmt.Println("v:", a.v)
+	fmt.Println("z_B:", a.z_B)
+	fmt.Println("matrix identical:", a.matrix_a == d.matrix_a)
+	fmt.Println("")
+
+	fmt.Println("Bob Information:")
+	fmt.Println("y:", b.y)
+	fmt.Println("s:", b.s)
+	fmt.Println("u:", b.u)
+	fmt.Println("matrix identical:", b.matrix_b == d.matrix_b)
+	fmt.Println("")
 
 	fmt.Println(a.computeOutput())
 }
